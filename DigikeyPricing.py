0246fac2bd7398e59e59df5bda75e787e6ac0c35
Daniel Higgins <80534190+DanielH208@users.noreply.github.com>
2023-03-30 20:20:38 +0100

    Added Try except block to handle unmatched items
    Added a try except block to handle any HTTP errors from the items that don't match the Digikey data.
    **PLACEHOLDER** in place of private credentials.

--- DigikeyPricing.py
+++ DigikeyPricing.py
@@ -68,23 +68,43 @@ def pricing (file, quantity):
 		print("refresh token: " + refresh_token)
 		print("access token: " + access_token)
 
-		for item in specific_columns:
-			get_headers = {"Authorization":"Bearer "+access_token,
-						   "X-DIGIKEY-Client-Id":client_id,
-						   "X-DIGIKEY-Locale-Site":"UK",
-						   "X-DIGIKEY-Locale-Language":"en",
-						   "X-DIGIKEY-Locale-Currency":"GBP",
-						   "X-DIGIKEY-Locale-ShipToCountry":"uk",
-						   "X-DIGIKEY-Customer-Id":"0"}
 
+		
+		for item in specific_columns:
+			# Set headers so the Digikey API returns the UK prices 
+			get_headers = {"Authorization":"Bearer "+access_token,
+							"X-DIGIKEY-Client-Id":client_id,
+							"X-DIGIKEY-Locale-Site":"UK",
+							"X-DIGIKEY-Locale-Language":"en",
+							"X-DIGIKEY-Locale-Currency":"GBP",
+							"X-DIGIKEY-Locale-ShipToCountry":"uk",
+							"X-DIGIKEY-Customer-Id":"0"}
+
+			# Query to only include the standar pricing values in the returned data
 			params = {"includes": "DigiKeyPartNumber,StandardPricing"}
 
-			item_url = "https://api.digikey.com/Search/v3/Products/" + item[1]
-			get_response = requests.get(item_url, headers=get_headers, params=params)
-			get_extract = get_response.json()
-			print(get_extract)
-			print(item_url)
+			try:
+				# Setting the URL path to change for each seperate item/product
+				item_url = "https://api.digikey.com/Search/v3/Products/" + item[1]
+				# Assemble the custom URL 
+				get_response = requests.get(item_url, headers=get_headers, params=params)
+				# Return a Json object of the result of get_response
+				get_extract = get_response.json()
+				print(get_extract)
+				print(item_url)
+				# Raise the error so i can handle it below in the except block
+				get_response.raise_for_status()
+			
+			# Handle HTTPError thrown from item stock code not corresponding with any item stock codes on Digikey API side
+			except requests.exceptions.HTTPError as err:
+				print(" ")
+				print("HTTP Error")
+				print(" ")
+				# Add the unmatched item to the unmatched componets list to be returned at the end of the program
+				unmatched_components.append(item)
+				
 
+	print(unmatched_components)
 		
 		
 	
