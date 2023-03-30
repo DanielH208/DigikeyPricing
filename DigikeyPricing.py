38200e9caf4faa13ea3040ad39b217abef1a03dd
Daniel Higgins <80534190+DanielH208@users.noreply.github.com>
2023-03-30 12:40:42 +0100

    Added command line functionality
    Added functionality so the user can run the program from the command line and pass in the arguments

--- DigikeyPricing.py
+++ DigikeyPricing.py
@@ -1,43 +1,50 @@
+
 # Python program to read CSV file without header
 
 # Import necessary packages
 import csv
 ###
-# Open file
-with open(r'D:\3adesign\DigikeyPricing\DigikeyPricing\Bill Of Materials PowerPortMax-v5.csv') as file_obj:
-	
-	# Skips the heading
-	# Using next() method
-	heading = next(file_obj)
-	
-	# Create reader object by passing the file
-	# object to reader method
-	reader_obj = csv.reader(file_obj)
-	
-	specific_columns = []
+import sys
 
-	# Iterate over each row in the csv file
-	# using reader object
-	for row in reader_obj:
-		specific_columns.append([row[1],row[4]])
+
+
+    
+
+def pricing (file, quantity):
+	# Open file
+	with open(file) as file_obj:
+	
+
 		
-	print(specific_columns)
+		# Skips the heading
+		# Using next() method
+		heading = next(file_obj)
+	
+		# Create reader object by passing the file
+		# object to reader method
+		reader_obj = csv.reader(file_obj)
+	
+		specific_columns = []
+
+		# Iterate over each row in the csv file
+		# using reader object
+		# Append the quantity and stock code value of each row into the specific columns list
+		for row in reader_obj:
+			specific_columns.append([row[1],row[4]])
+
+		
+		
+		print(specific_columns)
+		print(quantity)
+
+		overall_price = 0
+		unmatched_components = []
 
 
 
-#import pandas as pd
-#import numpy as np
+	
 
-#df=pd.read_csv(r'D:\3adesign\DigikeyPricing\DigikeyPricing\Bill Of Materials PowerPortMax-v5.csv')
-#print("The dataframe is:")
-#print(df)
-#specific_columns=df[["Quantity","Stock Code"]]
 
-#print("The column are:")
-#print(specific_columns)
 
-#for item in specific_columns:
-#	print(item["Quantity"])
-#	print(item["Stock Code"])
-
-#while specifc_columns[i] <= specifc_columns.length:
+if __name__ == '__main__':
+    pricing (*sys.argv[1:])
