5e9c7b73f3f81bdae8a7379a4743c571dbfa2f22
Daniel Higgins <80534190+DanielH208@users.noreply.github.com>
2023-03-30 17:08:16 +0100

    Added get request for each item
    Added a for loop that iterates through each item and sends a get request consisting of headers, custom URL for each item and a parameter query that filters the received data to only the standard pricing data for that item. 
    **PLACEHOLDER** is used to replace private credentials for security reasons.

--- DigikeyPricing.py
+++ DigikeyPricing.py
@@ -46,10 +46,11 @@ def pricing (file, quantity):
 		
 		
 		refresh_token = "**PLACEHOLDER**"
+		client_id = "**PLACEHOLDER**"
 
 		url = "https://api.digikey.com/v1/oauth2/token"
 
-		post_data = {"client_id":"**PLACEHOLDER**",
+		post_data = {"client_id":client_id,
 					 "client_secret":"**PLACEHOLDER**",
 					 "refresh_token":refresh_token,
 					 "grant_type":"refresh_token"}
@@ -66,7 +67,23 @@ def pricing (file, quantity):
 		access_token = extract['access_token']
 		print("refresh token: " + refresh_token)
 		print("access token: " + access_token)
-		
+
+		for item in specific_columns:
+			get_headers = {"Authorization":"Bearer "+access_token,
+						   "X-DIGIKEY-Client-Id":client_id,
+						   "X-DIGIKEY-Locale-Site":"UK",
+						   "X-DIGIKEY-Locale-Language":"en",
+						   "X-DIGIKEY-Locale-Currency":"GBP",
+						   "X-DIGIKEY-Locale-ShipToCountry":"uk",
+						   "X-DIGIKEY-Customer-Id":"0"}
+
+			params = {"includes": "DigiKeyPartNumber,StandardPricing"}
+
+			item_url = "https://api.digikey.com/Search/v3/Products/" + item[1]
+			get_response = requests.get(item_url, headers=get_headers, params=params)
+			get_extract = get_response.json()
+			print(get_extract)
+			print(item_url)
 
 		
 		
